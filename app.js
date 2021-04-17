36df7be7e72097953ffc6f9700e24a5b04c1743a
Projetphp <alexandre.capaldi@hotmail.fr>
2021-04-17 16:14:20 +0200

    iteration 3

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -23,7 +24,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/beers', (req, res) => {
-  res.render('beers');
+
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', {
+        beers: beersFromApi
+      });
+    })
+    .catch(error => console.log(error));
 });
 
 app.get('/random-beers', (req, res) => {
